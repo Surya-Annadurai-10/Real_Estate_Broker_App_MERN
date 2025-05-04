8580649fce1827fc410db99cfe931fb333c162a8
Surya Annadurai <suryaannadurai1041999@gmail.com>
2025-05-05 00:18:38 +0530

    homepage hero section done

--- Frontend/react_vite/src/Components/Header.jsx
+++ Frontend/react_vite/src/Components/Header.jsx
@@ -3,67 +3,99 @@ import logo from "../assets/royal.png";
 import { GoSearch } from "react-icons/go";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { FaSun } from "react-icons/fa";
+import { motion } from "motion/react";
 
 const Header = () => {
-  const stateUser  = useSelector(state => state.user);
-  const [searchTerm , setSearchTerm] = useState("");
+  const stateUser = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
-  console.log(stateUser , "Stateuser");
+  console.log(stateUser, "Stateuser");
 
   const handleSubmit = (e) => {
-   e.preventDefault();
+    e.preventDefault();
 
-   const urlParams = new URLSearchParams(window.location.search);
-   urlParams.set("searchTerm" , searchTerm);
-   const searchQuery = urlParams.toString();
-   navigate(`/search?${searchQuery}`)
-   }
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
 
-   useEffect(() =>{
-      const urlParams = new URLSearchParams(location.search);
-      const searchParams = urlParams.get("searchTerm");
-      if(searchParams){
-        setSearchTerm(searchParams)
-      }
-   },[location.search])
-  
-  
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchParams = urlParams.get("searchTerm");
+    if (searchParams) {
+      setSearchTerm(searchParams);
+    }
+  }, [location.search]);
+
   return (
-    <header className="w-full h-[10vh] bg-[#E2E8F0] px-10 flex items-center justify-between">
+    <header className="w-full h-[10vh] bg-[#E2E8F0]  flex items-center justify-evenly">
       <div>
-        <div className="flex items-center  flex-col">
-          <img className="w-[100px]" src={logo} alt="" />
-          <h1 className="logo  text-md">Estates</h1>
-        </div>
+        <Link to={"/"} className="flex items-center  flex-col">
+          <div className="flex items-center justify-center">
+            <motion.div
+              animate={{
+                rotate: 360,
+                yoyo: true,
+                transition: {
+                  duration: 2,
+                  repeat: Infinity,
+                  ease: "linear",
+                },
+              }}
+            >
+              <FaSun className="text-4xl  text-slate-700" />
+            </motion.div>
+            <h1 className="text-2xl font-bold text-slate-500">
+              Sun<span className="text-slate-700 font-bold">Estates</span>
+            </h1>
+          </div>
+        </Link>
       </div>
-      <div className="w-[33%]">
+      <div className="w-[17%]">
         <form className="flex items-center w-[100%] bg-[#F1F5F9] h-[45px] justify-center rounded-md">
-            <input onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search" className="w-[90%] p-2 h-[100%] outline-none border-none" type="text" />
-            <GoSearch onClick={(e) => handleSubmit(e)} className="p-1.5 hover:bg-[#E2E8F0] hover:cursor-pointer hover:scale-[1.2] transition-all active:scale-[0.9]  rounded-full" style={{fontSize:"2.3rem" }} />
+          <input
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search"
+            className="w-[90%] p-2 h-[100%] outline-none border-none"
+            type="text"
+          />
+          <GoSearch
+            onClick={(e) => handleSubmit(e)}
+            className="p-1.5 hover:bg-[#E2E8F0] hover:cursor-pointer hover:scale-[1.2] transition-all active:scale-[0.9]  rounded-full"
+            style={{ fontSize: "2.3rem" }}
+          />
         </form>
-       
       </div>
       <div className="flex items-center justify-center gap-10">
         <Link>
-        <h1 className=" ">Home</h1>
+          <h1 className="font-semibold ">Home</h1>
         </Link>
         <Link>
-        <h1 className=" ">About</h1>
+          <h1 className="font-semibold ">About</h1>
         </Link>
         {console.log(stateUser)}
 
-       {  
-        stateUser.userData ? 
-       <Link to={`/profile/${stateUser.userData._id}`}> <img className="w-[40px] h-[40px] rounded-full" src={stateUser.userData.avatar} alt="" /> </Link>
-        :
-        <>
-         <Link to={"/login"} className="w-[100px] h-[40px] grid place-items-center rounded-md bg-slate-700 text-white">
-           Login
-        </Link>
-        </>
-       }
-   
-       
+        {stateUser.userData ? (
+          <Link to={`/profile/${stateUser.userData._id}`}>
+            {" "}
+            <img
+              className="w-[40px] h-[40px] rounded-full"
+              src={stateUser.userData.avatar}
+              alt=""
+            />{" "}
+          </Link>
+        ) : (
+          <>
+            <Link
+              to={"/login"}
+              className="w-[100px] h-[40px] grid place-items-center rounded-md bg-slate-700 text-white"
+            >
+              Login
+            </Link>
+          </>
+        )}
       </div>
     </header>
   );
--- Frontend/react_vite/src/Containers/Home.jsx
+++ Frontend/react_vite/src/Containers/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main className='w-[100%] bg-[#F1F5F1]  min-h-[100vh]'>
+      {/* top */}
+      <div className='w-[100%] h-[55vh]  '>
+        <div className='w-[70%] mx-auto h-full gap-5 flex items-start justify-center flex-col'>
+          <h1 className='text-6xl font-bold text-slate-700 leading-17.5'>Find your next <span className='text-slate-500'>perfect</span> <br /> place with ease</h1>
+          <p className='text-slate-400 text-sm'>Sun Estates will help you find your home fast, easy and comfortable. <br /> Our expert support are always available.</p>
+          <Link to={"/search"} className={"text-slate-700 text-sm font-semibold"} >
+          <button className='px-6 py-4 bg-slate-700 text-[#F1F5F1] cursor-pointer hover:scale-[1.05] hover:bg-slate-600 transition-all active:scale-[0.95] rounded-md '>Let's Start now...</button></Link>
+      
+        </div>
+      </div>
+      {/* swiper */}
+      {/* last */}
+    </main>
   )
 }
 
