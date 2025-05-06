308566ae669da8db199ce87ade9b6b55e327a5a3
Surya Annadurai <suryaannadurai1041999@gmail.com>
2025-05-06 17:01:19 +0530

    bug fixed

--- Frontend/react_vite/src/Components/Footer.jsx
+++ Frontend/react_vite/src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900 relative flex md:flex-row flex-col items-center justify-center gap-1 md:gap-10 text-white text-center h-[60px] md:p-6 ">
     <p className='md:w-[50%] w-full  flex text-sm lg:text-md md:absolute left-5 items-center justify-center md:justify-start'>&copy; 2025 The SunEsates. All rights reserved.</p>
-    <p className='w-[50%%] flex  text-sm lg:text-md  items-center justify-start'>Made with by ♥️ by Surya Annadurai</p>
+    <p className='w-[50%%] flex  text-sm lg:text-md  items-center justify-start'>Made with ♥️ by Surya Annadurai</p>
     <a href='#' className=' rounded-full fixed z-100 bottom-3 right-3 shadow-red-500  flex items-center justify-end'>
       <button className='w-[50px] border border-gray-300 h-[50px] grid place-items-center z-[200] cursor-pointer bg-slate-900 rounded-full'>
         <MdOutlineVerticalAlignTop className='p-1 text-[2.5rem]' />
